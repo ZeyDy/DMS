518b3c69bc4d98c1dc7c926e76016a5f3b08abe2
Aretas Ubartas <aretas.ubartas@ips.lt>
2026-03-04 21:23:23 +0200

    /customers page

---
+++ document-generator/src/layout/MainLayout.tsx
@@ -0,0 +1,39 @@
+import { Layout, Menu } from "antd";
+import { Outlet, useNavigate } from "react-router-dom";
+
+const { Header, Sider, Content } = Layout;
+
+export const MainLayout = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider>
+        <Menu
+          theme="dark"
+          mode="inline"
+          onClick={({ key }) => navigate(key)}
+          items={[
+            { key: "/", label: "Home" },
+            { key: "/customers", label: "Customers" },
+            ]}
+        />
+      </Sider>
+
+      <Layout>
+        <Header
+          style={{
+            color: "white",
+            fontSize: 18,
+          }}
+        >
+          Document Generator
+        </Header>
+
+        <Content style={{ padding: 24 }}>
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
